f7c1f5d6db513f79ee085399aeb4c65764821102
David Mórász <m@mcro.de>
2023-11-22 23:57:56 +0100

    dynamic import irmagraph

--- js/md/IrmaGraph.jsx
+++ js/md/IrmaGraph.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import MdLazyLoad from 'echweb-shared/MdLazyLoad';
 import DisableWhileResizing from 'echweb-shared/DisableWhileResizing';
-import IrmaGraph from '../IrmaGraph/Render';
 import Utils from 'echweb-shared/Utils';
+import {Gh1, Gh2} from 'echweb-shared/Gh';
+
+const IrmaGraph = React.lazy(() => import('../IrmaGraph/Render'));
 
 export default function IrmaGraphExt(props)
 {
     return (
         <MdLazyLoad>
-            <DisableWhileResizing>
-                <IrmaGraph />
-            </DisableWhileResizing>
+            <React.Suspense fallback={
+                <div className="mdpdf invalid"><Gh1 glitchtype={1}>♾️</Gh1></div>
+            }>
+                <DisableWhileResizing>
+                    <IrmaGraph />
+                </DisableWhileResizing>
+            </React.Suspense>
         </MdLazyLoad>
     )
 }
